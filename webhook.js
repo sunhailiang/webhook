3976ba629ad4a4f4f741a7b9eda5eacfdbb6bb24
ËﺣÁÁ <sunhl@Fosun.com>
2020-04-18 17:05:54 +0800

    签名校验

--- webhook.js
+++ webhook.js
@@ -1,7 +1,26 @@
 let http = require("http")
+let crypto = require('crypto')
+let secret = "6239528"
+// 加密签名算法
+function sign (body) {
+  return `sha1` + crypto.createHmac('sha1', secret).update(body).digest("hex")
+}
 let server = http.createServer(function (req, res) {
   console.log("webhook req", req.method, req.url);
   if (req.method == 'POST' && req.url == '/webhook') {
+    let buffers = []
+    req.on("data", function (buffer) {
+      buffers.push(buffer)
+    })
+
+    req.on(end, function (buffer) {
+      let body = Buffer.concat(buffer)
+      let event = req.header["x-github-event"]; // event类型我们选的push
+      let signature = req.headers['x-hub-signature'] // 校验签名
+      if (signature !== sign(body)) {
+        return res.end("Not Allowed")
+      }
+    })
     res.setHeader("Content-Type", "application/json")
     res.end(JSON.stringify({ ok: true }))
   } else {
