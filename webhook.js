0b87d0848de3b7bedf07e383bb5e9455c8e528c7
ËﺣÁÁ <sunhl@Fosun.com>
2020-04-20 17:59:01 +0800

    error

--- webhook.js
+++ webhook.js
@@ -15,7 +15,7 @@ let server = http.createServer(function (req, res) {
     })
 
     req.on('end', function (buffer) {
-      let body = Buffer.concat(buffer)
+      let body = Buffer.concat(buffers)
       let event = req.headers["x-github-event"]; // event类型我们选的push
       let signature = req.headers['x-hub-signature'] // 校验签名
       if (signature !== sign(body)) {
